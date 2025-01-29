98e0594397d94dfd391321cd4ede219d64bde3cc
Chirag-Jian009 <Chiragjain009@gmail.com>
2025-01-29 14:54:53 +0530

    CHirag

---
+++ App1.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const fspromise = require("fs/promises");
+const newServer = http.createServer(async (req, res) =>{
+    
+         if(req.url=="/")
+         {
+            const data = await fspromise.readFile("./homepage.html");
+            res.end(data);
+         
+         }
+         else  if(req.url=="/about")
+         {
+                const data = await fspromise.readFile("./About.html");
+                res.end(data); 
+         } else {
+                const data = await fspromise.readFile("./error.html");
+                res.end(data); 
+         }
+         
+         
+  }
+
+);    
+newServer.listen(2500, () => console.log("---"));
